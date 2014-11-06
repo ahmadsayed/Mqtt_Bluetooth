651ae2aec15978f1a29b5a25092c01c4a0e7c83d
root <ahmad.ahmadsayed@gmail.com>
2014-11-06 05:33:47 +0200

    first commit

---
+++ examples/testMqttlib.ino
@@ -0,0 +1,33 @@
+#include <SoftwareSerial.h>
+#include <MqttSerial.h>
+#include <JsonParser.h>
+
+
+SoftwareSerial bluetoothSerial(10,11);
+MqttSerial mqttSerial ("arduino_lib",bluetoothSerial);
+void callback(String, String) ;
+
+void setup() {
+  Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  Serial.println("Serial initialized");
+  bluetoothSerial.begin(9600);
+  pinMode(13, OUTPUT);
+  mqttSerial.begin("broker.mqttdashboard.com", "8000", "test", "test");  
+  mqttSerial.connect();
+  mqttSerial.publish("testing", "testing123");
+  mqttSerial.subscribe("helloout");
+  mqttSerial.subscribe("helloout2");
+  
+
+}
+void callback(String topic, String payload) {'
+  if (topic == "helloout") digitalWrite(13, HIGH);
+  if (topic == "helloout2") digitalWrite(13, LOW);
+  Serial.println(topic + " : " + payload);
+}
+void loop() {
+  mqttSerial.loop(callback);
+}
